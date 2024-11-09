983dfc69b55d2ac81a9d1fd5d8ec7d63a8de43fa
stonericefield <89579455+Ratsnake@users.noreply.github.com>
2024-11-09 19:33:25 +0900

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,30 +1,26 @@
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip(60, 13, NEO_GRB + NEO_KHZ800);
+#define NUM 60
+
+Adafruit_NeoPixel strip(NUM, 13, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   strip.begin();
 
-  
+  int i = 0;
+
+  for(i = 0; i < NUM; i++){
+    strip.setPixelColor(i, strip.Color(50, 50, 50));
+  }
+
+  strip.show();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  int i = 0;
-
-  for(i = 0; i < 63; i++){
-    strip.setPixelColor(i - 3, strip.Color(0, 0, 0));
-    strip.setPixelColor(i - 2, strip.Color(50, 50, 50));
-    strip.setPixelColor(i - 1, strip.Color(255, 255, 255));
-    strip.setPixelColor(i, strip.Color(50, 50, 50));
-
-    strip.show();
-    delay(30);
-  }
-
-  delay(1000);
+  
 
 }
