07e9fc89d397459c5413cc207a88e8329aa77856
stonericefield <89579455+Ratsnake@users.noreply.github.com>
2024-11-10 17:06:21 +0900

    add static light pattern

---
+++ static_light/static_light.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h>
+
+#define NUM 60
+
+Adafruit_NeoPixel strip(NUM, 13, NEO_GRB + NEO_KHZ800);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+
+  int brightness = 30;
+
+  int i;
+
+  for(i = 0; i < NUM; i++){
+    strip.setPixelColor(i, strip.Color(brightness, brightness, brightness));
+  }
+
+  strip.show();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+
+}
