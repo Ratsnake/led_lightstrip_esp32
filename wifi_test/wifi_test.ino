06137ad1329716f2d7e91411e9b74d5a41edb7c4
stonericefield <89579455+Ratsnake@users.noreply.github.com>
2024-11-11 18:25:27 +0900

    WiFi越しに明るさを変更できるよう変更(？)
    ユーザーエクスペリエンスはクソ

--- wifi_test/wifi_test.ino
+++ wifi_test/wifi_test.ino
@@ -72,6 +72,16 @@ void loop() {
               }
               strip.show();
             }
+            else if(header.indexOf("GET /brightness=") >= 0){
+              unsigned short brightness = header.substring(16).toInt();
+              Serial.print("Brightness set : ");
+              Serial.println(brightness);
+              
+              for(int i = 0; i < SIZE; i++){
+                strip.setPixelColor(i, strip.Color(brightness, brightness, brightness));
+              }
+              strip.show();
+            }
 
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name = \"viewport\" content = \"width = device-width, initial-scale = 1\">");
@@ -82,7 +92,18 @@ void loop() {
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #5555FF; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #FF5555;}</style></head>");
+            client.println(".button2 {background-color: #FF5555;}");
+
+            client.println(".container { max-width: 500px; max-width: 500px; padding: 20px; font-family: sans-serif;}");
+            client.println(".slider-group { margin-bottom: 30px; }");
+            client.println("input[type=\"range\"] { -webkit-appearance: none; width: 100%; background: transparent; }");
+            client.println("input[type=\"range\"]::-webkit-slider-runnable-track { width: 100%; height: 8px; background: #ddd; border-radius: 4px; cursor: pointer; }");
+            client.println("input[type=\"range\"]::-webkit-slider-thumb { -webkit-appearance: none; height: 20px; width: 20px; border-radius: 50%; background: #007bff; cursor: pointer; margin-top: -6px; transition: background 0.2s; }");
+            client.println(".value-display { margin: 10px 0; font-size: 16px; }");
+            client.println(".send-link { display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none; border-radius: 4px; margin-top: 10px;}");
+            client.println(".send-link:hover { background-color: #0056b3}");
+
+            client.println("</style></head>");
 
             client.println("<body><h1>ESP32_Test</h1>");
 
@@ -93,7 +114,10 @@ void loop() {
             else{
               client.println("<p>strip is ON</p>");
               client.println("<p><a href = \"off\"><button class = \"button button2\">change off?</button></a></p>");
+              client.println("<div class=\"container\"><div class=\"slider-group\"><div class=\"value-display\">currentvalue:<span id=\"currentValue\">50</span></div><input type=\"range\" id=\"slider\" min=\"0\" max=\"100\" value=\"50\" oninput=\"updateValue()\"><div><a href=\"brightness=50\" id = \"sendLink\" class = \"send-link\">send data</a></div></div>");
             }
+
+            client.println("<script>function updateValue() { const slider = document.getElementById('slider'); const value = slider.value; const link = document.getElementById('sendLink'); const display = document.getElementById('currentValue'); display.textContent = value; link.href = `brightness=${value}`; }</script>");
 
             client.println("</body></html>");
 
